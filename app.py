e12d01ae8ffa8586acf497a8d93fe8e908417803
naidusaladi <79361491+naidusaladi@users.noreply.github.com>
2023-03-26 18:01:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pickle
 import math
-reg=pickle.load(open("C:\\Users\\DELL\\Desktop\\Untitled Folder\\linearreg.sav",'rb'))
+reg=pickle.load(open("linearreg.sav",'rb'))
 
 
 
